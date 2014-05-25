d966d18a98a3e7384a526fe37cdadf18eb779c75
shardul.chiplunkar <shardul.chiplunkar@83a50805-d1b6-c788-a754-61f2260299cb>
2014-05-25 13:28:52 +0000

    first release of VoteCounter with server/client model

--- AVR/lib/timerlib.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMER_LIB
-#define TIMER_LIB
-
-#include <avr/io.h>
-#include "timer0.h"
-#include "timer1.h"
-#include "timer2.h"
-
-#endif
--- AVR/muktangan/led3/src/common.c
+++
@@ -1,8 +0,0 @@
-#include <avr/io.h>
-#include "common.h"
-
-uint8_t full_buf[FULL_X_SIZE][FULL_Y_SIZE];
-uint8_t disp_buf[DISP_X_SIZE][DISP_Y_SIZE];
-
-uint8_t x_off = 0, y_off = 0;
-
