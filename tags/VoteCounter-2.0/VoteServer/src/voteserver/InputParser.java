b68992eed2267e5bd0631d8e09de933c0783c3dc
shardul.chiplunkar <shardul.chiplunkar@83a50805-d1b6-c788-a754-61f2260299cb>
2014-05-25 13:28:52 +0000

    first release of VoteCounter with server/client model

---
+++ tags/VoteCounter-2.0/VoteServer/src/voteserver/InputParser.java
@@ -0,0 +1,158 @@
+/**
+ * InputParser.java: parses XML input
+ * Copyright (C) 2012 - 2014 Shardul C.
+ *
+ * This file is part of VoteCounter.
+ *
+ * VoteCounter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VoteCounter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VoteCounter.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Bugs, tips, suggestions, requests to <shardul.chiplunkar@gmail.com>
+ * or <mjuvekar7@gmail.com>.
+ */
+package voteserver;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.jdom2.*;
+import org.jdom2.input.*;
+
+/**
+ * XML input parser for {@link VoteCounter}.
+ *
+ * This class provides methods for parsing an XML input file and converting the
+ * data into a format usable by the vote counting application. Currently, all
+ * data is being entered by hand (hard-coded): the program would be more
+ * flexible and useful for many others if the data could be in a predefined
+ * format, such as XML. We are working towards making XML files the primary mode
+ * of input to the program.
+ *
+ * @author shardul
+ */
+public class InputParser {
+    private List<String> groups = new ArrayList<>();
+    private List<String> genericPosts = new ArrayList<>();
+    private List<List<String>> genericNominees = new ArrayList<>();
+    private List<String> nonGenericPosts = new ArrayList<>();
+    private List<List<List<String>>> nonGenericNominees = new ArrayList<>();
+
+    /**
+     * Parse the XML input file and get groups, posts, and nominees.
+     *
+     * The {@code parse} method parses the document using
+     * {@link org.jdom2.input.SAXBuilder} which builds a pseudo-tree structure
+     * representing the XML file. The method then traverses each element and
+     * builds a representation of the data that makes sense to the vote-counting
+     * program; that is, it organizes the data into posts and groups as
+     * specified in the input file.
+     *
+     * The behavior of the {@code parse} method is unspecified if the XML file
+     * is not structured as it expects it to be. This can happen even if the
+     * syntax is all right, because of mispelings or order elements of incorrect
+     * the. A format specifier file is coming soon.
+     *
+     * @param is an {@link java.io.InputStream} for the XML input file
+     * @throws JDOMException if XML structure/syntax is incorrect
+     * @throws IOException if error accessing file
+     */
+    public void parse(java.io.InputStream is) throws JDOMException, IOException {
+        Element root = ((new SAXBuilder()).build(is)).getRootElement();
+
+        List<Element> groupList = root.getChild("groups").getChildren();
+        for (int i = 0; i < groupList.size(); i++) {
+            groups.add(groupList.get(i).getText());
+        }
+
+        // make this look like the non-generic stuff (much neater)
+
+        List<Element> genericPostElements = root.getChild("posts").getChild("generic").getChildren();
+        for (int i = 0; i < genericPostElements.size(); i++) {
+            genericPosts.add(genericPostElements.get(i).getAttributeValue("name"));
+            genericNominees.add(new ArrayList<String>());
+            List<Element> currentNominees = genericPostElements.get(i).getChildren();
+            for (int j = 0; j < currentNominees.size(); j++) {
+                genericNominees.get(i).add(currentNominees.get(j).getText());
+            }
+        }
+
+        // you better document this stuff NOW
+        // or else it'll work, but no-one will know HOW
+
+        List<Element> nonGenericPostElements = root.getChild("posts").getChild("nongeneric").getChildren();
+        for (int i = 0; i < nonGenericPostElements.size(); i++) {
+            nonGenericPosts.add(nonGenericPostElements.get(i).getAttributeValue("name"));
+            nonGenericNominees.add(new ArrayList<List<String>>());
+            List<Element> currentGroups = nonGenericPostElements.get(i).getChildren();
+            for (int j = 0; j < currentGroups.size(); j++) {
+                nonGenericNominees.get(i).add(new ArrayList<String>());
+                List<Element> currentNominees = currentGroups.get(j).getChildren();
+                for (int k = 0; k < currentNominees.size(); k++) {
+                    nonGenericNominees.get(i).get(j).add(currentNominees.get(k).getText());
+                }
+            }
+        }
+    }
+
+    /**
+     * Get nominees for generic posts.
+     *
+     * The return value is a two-dimensional list, with the first dimension
+     * representing posts and the second, individual nominees.
+     *
+     * @return nominees for generic posts
+     */
+    public List<List<String>> getGenericNominees() {
+        return genericNominees;
+    }
+
+    /**
+     * Get nominees for non-generic posts.
+     *
+     * The return value is a three-dimensional list, with the first dimension
+     * representing posts, the second, groups, and the third, individual
+     * nominees.
+     *
+     * @return nominees for non-generic posts
+     */
+    public List<List<List<String>>> getNonGenericNominees() {
+        return nonGenericNominees;
+    }
+
+    /**
+     * Get list of nominee groups.
+     *
+     * @return list of nominee groups
+     */
+    public List<String> getGroups() {
+        return groups;
+    }
+
+    /**
+     * Get list of generic (non-group) posts.
+     *
+     * @return list of generic posts
+     */
+    public List<String> getGenericPosts() {
+        return genericPosts;
+    }
+
+    /**
+     * Get list of non-generic (group-wise) posts.
+     *
+     * @return list of non-generic posts
+     */
+    public List<String> getNonGenericPosts() {
+        return nonGenericPosts;
+    }
+}
---
+++ tags/VoteCounter-2.0/VoteServer/src/voteserver/VoteServerThread.java
@@ -0,0 +1,136 @@
+/*
+ * VoteServerThread.java: vote-counting server thread
+ * Copyright (C) 2012 - 2014 Shardul C.
+ *
+ * This file is part of VoteCounter.
+ *
+ * VoteCounter is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * VoteCounter is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * VoteCounter. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Bugs, tips, suggestions, requests to <shardul.chiplunkar@gmail.com>
+ * or <mjuvekar7@gmail.com>.
+ */
+package voteserver;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * Server thread to handle a single client.
+ *
+ * {@code VoteServerThread} is created and started by {@link VoteServer} when it
+ * accepts a client connection. {@code VoteServerThread} then creates I/O
+ * streams, sends required data, and begins receiving votes from the client
+ * until the client terminates. Votes are updated and written on a regular basis
+ * so that they are not lost in case of an application crash.
+ *
+ * @author Shardul C.
+ */
+public class VoteServerThread extends Thread {
+    // input and output to/from client
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+
+    /**
+     * Construct a new {@code VoteServerThread} connected to the client.
+     *
+     * The client's connection has already been accepted and a corresponding
+     * {@code Socket} is passed to the thread. The I/O streams function through
+     * this {@code Socket}.
+     *
+     * @param sock a {@code Socket} to the client
+     */
+    public VoteServerThread(Socket sock) {
+        super("VoteServerThread");
+        try {
+            // create i/o streams
+            this.out = new ObjectOutputStream(sock.getOutputStream());
+            out.flush();
+            this.in = new ObjectInputStream(sock.getInputStream());
+        } catch (IOException ex) {
+            System.err.println("Caught IOException: " + ex.getLocalizedMessage());
+            System.exit(-2);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            // send data
+            out.writeObject(VoteServer.groups);
+            out.writeObject(VoteServer.genericPosts);
+            out.writeObject(VoteServer.nonGenericPosts);
+            int genSize = VoteServer.genericPosts.size();
+            int nonGenSize = VoteServer.nonGenericPosts.size();
+
+            // parts after splitting input from client
+            String parts[];
+            // current voter's group
+            String group;
+            // current voter's options
+            List<List<String>> options = new ArrayList<>();
+
+            // initialize the non-changing part of the voter's options (the generic parts)
+            for (int i = 0; i < genSize; i++) {
+                options.add(VoteServer.genericNominees.get(i));
+            }
+
+            while (true) {
+                out.reset();
+                // what group?
+                out.writeObject(messages.Messages.GET_GROUP);
+                group = in.readObject().toString();
+
+                // client terminated
+                if (group.equals(messages.Messages.GOODBYE.toString())) {
+                    System.out.println("client terminated");
+                    break;
+                }
+
+                int groupIndex = VoteServer.groups.indexOf(group);
+                // increment total and group voter numbers
+                VoteServer.voters[0]++;
+                VoteServer.voters[groupIndex + 1]++;
+                // copy the correct section of the non-generic posts/nominees
+                for (int i = 0; i < nonGenSize; i++) {
+                    options.add(VoteServer.nonGenericNominees.get(i).get(groupIndex));
+                }
+                // send options
+                out.writeObject(options);
+
+                // get votes
+                for (int step = 0; step < options.size(); step++) {
+                    parts = ((String) in.readObject()).split(" ");
+                    if (step < genSize) {
+                        VoteServer.incVotes(step, Integer.parseInt(parts[1]));
+                    } else {
+                        VoteServer.incVotes(step + groupIndex * nonGenSize, Integer.parseInt(parts[1]));
+                    }
+                }
+                VoteServer.writeVotes();
+                options.remove(genSize + nonGenSize - 1);
+                options.remove(genSize + nonGenSize - 2);
+            }
+            VoteServer.writeVotes();
+            in.close();
+            out.close();
+        } catch (IOException ex) {
+            System.err.println("Caught IOException: " + ex.getLocalizedMessage());
+            System.exit(-2);
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Caught ClassNotFoundException: " + ex.getLocalizedMessage());
+            System.exit(-3);
+        }
+    }
+}
